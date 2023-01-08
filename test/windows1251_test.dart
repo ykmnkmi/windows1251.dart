87fc9eea6c98938d2b40d37b3ef9b81b240fda8a
Olzhas Suleimen <amajisenpai@gmail.com>
2023-01-08 20:25:42 +0600

    Add chunked conversion tests

--- test/windows1251_test.dart
+++ test/windows1251_test.dart
@@ -1,3 +1,5 @@
+import 'dart:convert';
+
 import 'package:test/test.dart';
 import 'package:windows1251/windows1251.dart';
 
@@ -21,6 +23,34 @@ void main() {
 
     test('invalid', () {
       expect(() => windows1251.encode('\uFFFF'), throwsArgumentError);
+    });
+  });
+
+  group('chunked', () {
+    test('encode', () {
+      var buffer = <int>[];
+      var byteSink = ByteConversionSink.withCallback(buffer.addAll);
+
+      windows1251.encoder.startChunkedConversion(byteSink)
+        ..add('При')
+        ..add('вет')
+        ..add('!')
+        ..close();
+
+      expect(buffer, orderedEquals(bytes));
+    });
+
+    test('decoder', () {
+      var buffer = StringBuffer();
+      var stingSink = StringConversionSink.fromStringSink(buffer);
+
+      windows1251.decoder.startChunkedConversion(stingSink)
+        ..add(bytes.sublist(0, 3))
+        ..add(bytes.sublist(3, 6))
+        ..add(bytes.sublist(6))
+        ..close();
+
+      expect(buffer.toString(), equals('Привет!'));
     });
   });
 }
